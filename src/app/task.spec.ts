65615069e95f8826540f45ef1f7ae36d18e5fafc
recca0120 <recca0120@gmail.com>
2017-09-23 03:10:07 +0800

    撰寫 Task 需求，並完成程式

--- src/app/task.spec.ts
+++ src/app/task.spec.ts
@@ -1,7 +1,31 @@
 import { Task } from './task';
 
 describe('Task', () => {
-  it('should create an instance', () => {
-    expect(new Task()).toBeTruthy();
-  });
+    it('必須有名稱、日期、完成、刪除', () => {
+        const name = 'task';
+        const date = new Date;
+        const done = false;
+        const deleted = false;
+
+        const task = new Task(name, date, done, deleted);
+
+        expect(task.name).toBe(name);
+        expect(task.date).toBe(date);
+        expect(task.done).toBe(done);
+        expect(task.deleted).toBe(deleted);
+    });
+
+    it('日期、完成、刪除 給預設值', () => {
+        const name = 'task';
+        const date = new Date;
+        const done = false;
+        const deleted = false;
+
+        const task = new Task(name);
+
+        expect(task.name).toBe(name);
+        expect(task.date instanceof Date).toBeTruthy();
+        expect(task.done).toBe(done);
+        expect(task.deleted).toBe(deleted);
+    });
 });
--- src/app/task.ts
+++ src/app/task.ts
@@ -1,2 +1,13 @@
 export class Task {
+    name: string;
+    date: Date;
+    done: boolean;
+    deleted: boolean;
+
+    constructor(name: string, date: Date = new Date, done: boolean = false, deleted: boolean = false) {
+        this.name = name;
+        this.date = date;
+        this.done = done;
+        this.deleted = deleted;
+    }
 }
