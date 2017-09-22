e495c13b75e1e0c8d18522f73bab7e1ee6f14c36
recca0120 <recca0120@gmail.com>
2017-09-22 19:50:22 +0800

    spyOn setBar

--- src/app/spy.spec.ts
+++ src/app/spy.spec.ts
@@ -22,8 +22,13 @@ describe('spy', () => {
         expect(foo.getBar()).toBe('fooBar');
     });
 
-    it('spyOn', () => {
+    it('spyOn setBar', () => {
+        spyOn(foo, 'setBar');
 
+        foo.setBar('buzz');
+
+        expect(foo.getBar()).not.toBe('buzz');
+        expect(foo.getBar()).toBe('fooBar');
     });
 
 });
